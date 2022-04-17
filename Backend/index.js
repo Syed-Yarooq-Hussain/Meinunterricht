8a4177eb3ca49784c3e55cc92a4a0228e397bc34
Syed Yarooq <syed.yarooq@gmail.com>
2022-04-17 12:10:04 +0500

    change Backend port

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const movieController = require('./src/controller/MovieController')
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 
 app.use(cors({
